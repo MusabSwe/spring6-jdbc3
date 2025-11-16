16902232ea19668fd0033dc1e6a17972cfba38a7
musab <malqarninet@outlook.com>
2025-11-16 18:51:35 +0300

    Read all records from DB using JDBC and RowMapper with both JDBCTemplate and SimpleJDBCInsert Read a single record from DB using JDBC and keyHolder and with both JDBCTemplate and SimpleJDBCInsert

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
@@ -1,10 +1,18 @@
 package com.pluralsight.conference.repository;
 
 import com.pluralsight.conference.model.Speaker;
+import com.pluralsight.conference.repository.util.SpeakerRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 
 @Repository("speakerRepository")
@@ -17,18 +25,15 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
     }
 
     public List<Speaker> findAll() {
-        Speaker speaker = new Speaker();
-        speaker.setName("Bryan Hansen");
-        speaker.setSkill("Java");
-        List<Speaker> speakers = new ArrayList<>();
-        speakers.add(speaker);
+
+        List<Speaker> speakers = jdbcTemplate.query("select * from speaker", new SpeakerRowMapper());
         return speakers;
     }
 
     @Override
     public Speaker create(Speaker speaker) {
 //        method 1 of creating a record in the DB, using jdbcTemplate
-        jdbcTemplate.update("INSERT INTO speaker (name) values (?)", speaker.getName());
+//        jdbcTemplate.update("INSERT INTO speaker (name) values (?)", speaker.getName());
 
 //        method 2 of creating a record in the DB, using SimpleJdbcInsert
 //        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
@@ -45,6 +50,38 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
 //
 //        System.out.println(key);
 
-        return null;
+//        method 3 to create a recode with JDBCTemplate and return in the response the created object
+//        KeyHolder keyHolder = new GeneratedKeyHolder();
+//        jdbcTemplate.update(new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//                PreparedStatement ps = con.prepareStatement("INSERT INTO speaker (name) values (?)", new String[]{"id"});
+//                ps.setString(1, speaker.getName());
+//                return ps;
+//            }
+//
+//
+//        }, keyHolder);
+//        Number id = keyHolder.getKey();
+
+//        method4 simpleJDBCInsert with returned created object
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+        insert.setTableName("speaker");
+        List<String> cols = new ArrayList<>();
+        cols.add("name");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", speaker.getName());
+
+        insert.setGeneratedKeyName("id");
+
+        Number id = insert.executeAndReturnKey(data);
+
+        System.out.println(id);
+        return getSpeaker(id.intValue());
+    }
+
+    private Speaker getSpeaker(int id) {
+        return jdbcTemplate.queryForObject("select * from speaker where id = ?", new SpeakerRowMapper(), id);
     }
 }
